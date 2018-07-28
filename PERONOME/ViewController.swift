ab5b5eeb2c65946416b530573e1b7f4d8cf93ce2
佐川　晴海 <m0114187e3@edu.teu.ac.jp>
2018-07-28 13:17:12 +0900

    Initial Commit

---
+++ PERONOME/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PERONOME
+//
+//  Created by 佐川　晴海 on 2018/07/28.
+//  Copyright © 2018年 佐川　晴海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
