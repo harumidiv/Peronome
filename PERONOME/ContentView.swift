519f0cf40ee8b790f053f64c0fdf9624dabf6b6a
harumidiv <42649032+harumidiv@users.noreply.github.com>
2023-10-24 17:43:23 +0900

    SwiftUIのプロジェクトを配置

---
+++ PERONOME/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peronome
+//
+//  Created by 佐川 晴海 on 2023/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
